14068a58deb3181a0a9859cd124e924495cbc61f
vijaykumar <vijay1111mane@gmail.com>
2022-05-06 12:59:29 +0530

    commit;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular-loop';
+  // title = 'For Loop';
+  // users = ['Vijay','Prajwal','Kiran','Appu','Nikhil']
+  // userDetails = [
+  //   {name: 'Vijay', email:'vijay@gmail.com', phone:'0987654321'},
+  //   {name: 'Prajwal', email:'prajwal@gmail.com', phone:'5432109876'},
+  //   {name: 'Kiran', email:'kiran@gmail.com', phone:'0932187654'},
+  //   {name: 'Appu', email:'appu@gmail.com', phone:'0954321876'},
+  //   {name: 'Nikhil', email:'nikhil@gmail.com', phone:'0976543281'},
+  // ];
+
+  // Multidiamentional Array or Neated loop
+  title = 'Nested Loop';
+  users =[
+    {name:'Vijay', phone:'1234', socialAccounts:['facebook','insta','gmail']},
+    {name:'Anil', phone:'22222',socialAccounts:['Whatsapp','linkedin','sharchat']},
+    {name:'kumar', phone:'1111',socialAccounts:['Apna','Hirect','jodDrive']},
+  ]
 }
